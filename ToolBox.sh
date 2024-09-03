10290c42f3232f62e413bf8620e49cd454c9775e
Adam Lev <adamlev513@gmail.com>
2024-09-03 16:31:24 +0300

    Update ToolBox.sh

--- ToolBox.sh
+++ ToolBox.sh
@@ -61,7 +61,7 @@ echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] \
 https://apt.releases.hashicorp.com $(lsb_release -cs) main" | \
 sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt update
-sudo apt-get install terraform
+sudo apt-get install terraform -y
 
 echo "Installing kubectl"
 curl -LO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
